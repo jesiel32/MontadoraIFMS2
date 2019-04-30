249d370a80f8160f444a2d242ba5648f900b7782
05094805193 <05094805193@IFCBDM01.laboratorio.ifms.br>
2019-04-30 00:24:21 -0300

    Primeiro commit da aplicação

---
+++ src/java/br/com/locadora/mapeamento/pessoa/Pessoa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.locadora.mapeamento.pessoa;
+
+/**
+ *
+ * @author 05094805193
+ */
+public abstract class Pessoa {
+    private String nome;
+    private Integer idade;
+    private String cpf;
+    
+}
